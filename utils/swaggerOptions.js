a7fb20ca4e13654d8dd76cbf2539e237b1c330d0
Manassaharoy <triptoroy5@gmail.com>
2023-04-05 15:05:45 +0600

    up

--- utils/swaggerOptions.js
+++ utils/swaggerOptions.js
@@ -2,7 +2,7 @@ const swaggerOptions = {
   definition: {
     info: {
       title: "Nodejs with prisma template",
-      version: "1.0.0",
+      version: "1.1.0",
       description:
         "A starter code template for Node.js and Express with prisma ORM, error handling, and response middleware.",
     },
